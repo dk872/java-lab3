c415f436413daeb4d9f25b361ba7f56a4e263c2c
dk872 <dimakulyk2005@gmail.com>
2025-09-12 13:35:41 +0300

    Add AccumulatedCard class

---
+++ src/main/java/org/example/AccumulatedCard.java
@@ -0,0 +1,31 @@
+package org.example;
+
+class AccumulatedCard extends Card {
+    private double balance;
+    private final double tripCost;
+
+    public AccumulatedCard(String id, double initialBalance, double tripCost) {
+        super(id, "regular", "unlimited", 0, true, null);
+        this.balance = initialBalance;
+        this.tripCost = tripCost;
+    }
+
+    @Override
+    public boolean canPass() {
+        return isValid() && balance >= tripCost;
+    }
+
+    @Override
+    public void useTrip() {
+        if (!canPass()) return;
+        balance -= tripCost;
+    }
+
+    public void addBalance(double amount) {
+        balance += amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
