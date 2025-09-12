78165b60b674c942c6dbb44bfce082ba5e4dab1c
dk872 <dimakulyk2005@gmail.com>
2025-09-12 13:36:21 +0300

    Add Turnstile class

---
+++ src/main/java/org/example/Turnstile.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Turnstile {
+    private int passesAllowed = 0;
+    private int passesDenied = 0;
+    private final Map<String, Integer> passesByType = new HashMap<>();
+    private final Map<String, Integer> deniedByType = new HashMap<>();
+    private final double tripCost;
+
+    public Turnstile(double tripCost) {
+        this.tripCost = tripCost;
+    }
+
+    public double getTripCost() {
+        return tripCost;
+    }
+
+    public boolean payTrip(Card card) {
+        if (card == null) {
+            passesDenied++;
+            return false;
+        }
+        if (card.canPass()) {
+            card.useTrip();
+            passesAllowed++;
+            passesByType.put(card.getType(), passesByType.getOrDefault(card.getType(), 0) + 1);
+            return true;
+        } else {
+            passesDenied++;
+            deniedByType.put(card.getType(), deniedByType.getOrDefault(card.getType(), 0) + 1);
+            return false;
+        }
+    }
+
+    public void printSummary() {
+        System.out.println("\nTotal permissions: " + passesAllowed);
+        System.out.println("Total rejections: " + passesDenied);
+    }
+
+    public void printByType() {
+        System.out.println("\nPermissions by type:");
+        for (String type : passesByType.keySet()) {
+            System.out.println(type + ": " + passesByType.get(type));
+        }
+        System.out.println("Rejections by type:");
+        for (String type : deniedByType.keySet()) {
+            System.out.println(type + ": " + deniedByType.get(type));
+        }
+    }
+}
